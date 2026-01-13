0413fc5a5d1bd20a27d6b5004ee2a286bf60be7d
Santosh Ramesh <edu.sanumaramy@gmail.com>
2026-01-12 22:28:41 -0800

    adding a public folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ka_rg/', // this must match your repository name
+  // base: '/ka_rg/', // this must match your repository name
+  base: '/', // this must match your repository name
 });
